6052830c46a0304425e7717e0056caac4ef78d0c
Tuscanny Polk <tuscannypolk@gmail.com>
2022-02-18 16:13:06 -0500

    made some text changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -20,15 +20,20 @@ function AboutMe() {
           as long as I can remember. In recent years, my creative endevors has
           expanded to Photography and coding. Ive always been curious as to how
           ideas and designs come together to make somthing interesting and
-          useful. I enjoy challenging myself to make art and design that is both
+          practical. I enjoy challenging myself to make art and design that is both
           aesticically pleasing and functional.{" "}
         </h1>
         <h1 className="about-me-text">
           In 2011, I graduated from Full Sail University. My time there taught
           me a buch about how to handle professional projects, and how
-          impoertant collaboration is when it comes to making an idea as strong
+          important collaboration is when it comes to making an idea as strong
           as it can be.{" "}
         </h1>
+        <h1 className="about-me-text">
+         All forms of art inspire my work. I think that its so important to reference art that inspires you and then add on to it with your own ideas so that you come up with something new and fresh. Some of my main ispirations are the design work at Apple, the works of Andy Warhol and anything relating to Kanye West.{" "}
+        </h1>
+
+
       </section>
     </div>
   );
