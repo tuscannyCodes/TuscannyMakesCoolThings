ebf4d6564385470020e5cd34be65e1ac7f4f512e
Tuscanny Polk <tuscannypolk@gmail.com>
2022-02-23 19:19:24 -0500

    fixed and updated marquee animation with framer motion

--- src/components/ScrollingHeader/ScrollingHeader.js
+++ src/components/ScrollingHeader/ScrollingHeader.js
@@ -1,22 +1,65 @@
+// import './ScrollingHeader.css';
+
+
+// function ScrollingHeader() {
+//     return (
+        
+// 	    <div class="marquee">
+// 			<div class="marquee__inner" >
+// 				<span> JACK OF ALL TRADES</span>
+// 				<span> JACK OF ALL TRADES</span>
+// 				<span> JACK OF ALL TRADES</span>
+// 				<span> JACK OF ALL TRADES</span>
+// 				<span> JACK OF ALL TRADES</span>
+// 				<span> JACK OF ALL TRADES</span>
+// 				<span> JACK OF ALL TRADES</span>
+// 			</div>
+// 			</div>
+//     );
+      
+//   };
+  
+//   export default ScrollingHeader;
+
+
+import React from "react";
+// 1. Importing framer-motion
+import { motion } from "framer-motion";
+
 import './ScrollingHeader.css';
 
+// 2. Defining Variants
+const marqueeVariants = {
+  animate: {
+    x: [0, -1335],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: "loop",
+        duration: 8,
+        ease: "linear",
+      },
+    },
+  },
+};
 
-function ScrollingHeader() {
-    return (
-        
-	    <div class="marquee">
-			<div class="marquee__inner" >
-				<span> JACK OF ALL TRADES</span>
-				<span> JACK OF ALL TRADES</span>
-				<span> JACK OF ALL TRADES</span>
-				<span> JACK OF ALL TRADES</span>
-				<span> JACK OF ALL TRADES</span>
-				<span> JACK OF ALL TRADES</span>
-				<span> JACK OF ALL TRADES</span>
-			</div>
-			</div>
-    );
-      
-  };
-  
-  export default ScrollingHeader;
+const ScrollingHeader = () => {
+  return (
+    <div>
+      <div className="marquee">
+                {/* 3. Using framer motion */}
+        <motion.div
+          className="track"
+          variants={marqueeVariants}
+          animate="animate"
+        >
+          <h1>
+           JACK OF ALL TRADES. JACK OF ALL TRADES.
+          </h1>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollingHeader;
