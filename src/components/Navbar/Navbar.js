32b43c0e5110ec747cb33b71520da3a7d0c0243f
Tuscanny Polk <tuscannypolk@gmail.com>
2022-03-06 21:00:39 -0500

    hamburger animation is working

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import './Navbar.css';
 import {useNavigate} from 'react-router-dom';
+import { motion } from 'framer-motion';
+import React, { useState, useCallback } from "react";
 
 export const ROUTES = {
     HOME: '/',
@@ -10,17 +12,31 @@ export const ROUTES = {
 
 function Navbar() {
     const navigate = useNavigate();
-
+    const [toggleDirection, setToggleDirection] = useState(-900) 
+    const toggleOn = () => {
+      setToggleDirection(toggleDirection === -500 ? 0 : -500)
+    
+    
+    }
     return (
       <>
-   
-      <nav className="navbar">
+         <motion.div onTap={toggleOn} className="HamburgerCard"
+    animate={{x:toggleDirection}}
+    >
+      <h1>hello</h1>
+      
+      </motion.div>
+
+
+      <nav className="navbar" onTap={toggleOn}>
            {/* hamburger start */}
-     <div className="nav-hamburger">
+     
+     <div className="nav-hamburger" onClick={(toggleOn)}>
 <div className="nav-hamburger-line"></div>
 <div className="nav-hamburger-line"></div>
 <div className="nav-hamburger-line"></div>
 </div>
+
    {/* hamburger end*/}
  
           <div onClick={() => {navigate(ROUTES.HOME)}} className="navbar-text">HOME</div>
