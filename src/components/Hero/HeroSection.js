0e116a82d7ae02f73517680113245194f1814cae
Tuscanny Polk <tuscannypolk@gmail.com>
2022-05-16 03:37:49 -0400

    updated layout icons and transitions

--- src/components/Hero/HeroSection.js
+++ src/components/Hero/HeroSection.js
@@ -21,7 +21,12 @@ function HeroSection() {
 
       <section id="heroTextContainer">
       <h1 className="hero-text">TUSCANNY</h1>
-      <h1 className="hero-text">MAKES COOL</h1>
+      <h1 className="hero-text">MAKES<motion.div 
+    initial={{translateX:2000}} 
+    animate={{translateX:0}}
+    exit={{translateX:900}} 
+    transition={{ type: "spring", stiffness: 110, mass: 2}}
+    > <span>COOL</span></motion.div></h1>
       <h1 className="hero-text">THINGS!</h1>
       <img class="icons" src="/icons.png"></img>
       </section>
@@ -31,8 +36,10 @@ function HeroSection() {
      
     </header>
 
-   
+    
+
     <RotatingImg />
+    
     </motion.div>
     
   );
--- src/components/RotatingImg/RotatingImg.js
+++ src/components/RotatingImg/RotatingImg.js
@@ -3,9 +3,11 @@ import './RotatingImg.css';
 
 function RotatingImg() {
     return (
+      
       <div className="RotatingImg-Container">
        <img className="hero-design"  src="/rotatingText.png" alt="design"></img>
     </div>
+    
     );
       
   };
